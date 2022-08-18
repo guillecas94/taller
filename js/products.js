005983be80fe3da7ce4b3c8b84398dc86d381631
Guillecas94 <21.guillecas@gmail.com>
2022-08-18 19:58:22 -0300

    Categoria Autos agregada

---
+++ js/products.js
@@ -0,0 +1,102 @@
+const CARTS_URL ="https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+let productsArray = [];
+
+
+function showProductsList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.products.length; i++){ 
+        let datosAutos = array.products[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+            <div class="col-3">
+                    <img src="` + datosAutos.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">
+                        <h4>`+ datosAutos.name +`</h4> 
+                        <p> `+ datosAutos.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + datosAutos.soldCount + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CARTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            showProductsList(productsArray);
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let productsArray = [];
+
+// function showProductsList(array){
+//     let htmlContentToAppend = "";
+
+//     for(let i = 0; i < array.products.length; i++){ 
+//         let datos = array.products[i];
+//         htmlContentToAppend += `
+//         <div class="list-group-item list-group-item-action">
+//             <div class="row">
+//                 <div class="col-3">
+//                     <img src="` + datos.image+ `" alt="product image" class="img-thumbnail">
+//                 </div>
+//                 <div class="col">
+//                     <div class="d-flex w-100 justify-content-between">
+//                         <div class="mb-1">
+//                         <h4>`+ datos.name +`</h4> 
+//                         <p> `+ datos.description+`</p> 
+//                         </div>
+//                         <small class="text-muted">` + datos.soldCount + ` artículos</small> 
+//                     </div>
+
+//                 </div>
+//             </div>
+//         </div>
+//         `
+//        document.getElementById("cats-products").innerHTML = htmlContentToAppend;
+
+//     }
+
+// document.addEventListener("DOMContentLoaded", function(e){
+//     getJSONData(CARTS_URL).then(function(resultObj){
+//         if (resultObj.status === "ok")
+//         {
+//             productsArray = resultObj.data;
+//             showProductsList(array); 
+//         }
+//     });
+// })}
